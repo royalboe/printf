4de2b4b3bfe5a39471b1397622072e89991d000d
speak2debby <speak2debby@gmail.com>
2022-04-17 21:50:49 +0100

    get_functions more update

--- get_func.c
+++ get_func.c
@@ -11,7 +11,7 @@ int (*get_func(char x))(va_list)
 	spec arr[] = {
 		{"c", print_c},
 		{"s", print_s},
-		{"%", print_percent},
+		{'%', print_percent},
 		{"d", print_d},
 		{"i", print_i},
 		{NULL, NULL}
