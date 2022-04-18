bbfb321ca1107b8d328e5869efc0fcd5fbbbb601
Ayomide <auomidejohnson@gmail.com>
2022-04-18 12:17:38 -0700

    Added the print_string function that takes a string as arg and prints it

--- functions.c
+++ functions.c
@@ -14,27 +14,23 @@ int print_c(va_list args)
 	return (_putchar(c));
 }
 /**
- * print_s - prints a string
- * @args: string  argument
+ * print_string - a function that prints a string
+ * @str: string  argument
  * Return: number of characters
  */
-int print_s(va_list args)
+int print_string(char *str)
 {
-	int i, count = 0;
-	char *str;
-
-	i = 0;
-	str = va_arg(args, char*);
-	if (str == NULL)
-		str = "(null)";
-	while (str[i] != '\0')
+	int count = 0;
+	
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
+		str++;
 		count++;
 	}
 	return (count);
 }
+
 /**
  * print_percent - pass the percent sing
  * @args: string  argument
