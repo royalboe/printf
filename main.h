c921b6bb0d16c83ea6a3466bea848da75ac32570
Royalboe <auomidejohnson@gmail.com>
2022-04-18 12:54:34 -0700

    Added more function prototypes

--- main.h
+++ main.h
@@ -13,12 +13,21 @@ typedef struct specifier
 	char *valid;
 	int (*f)(va_list);
 } spec;
-int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
+
 int _putchar(char c);
-int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
+int print_number(int n);
+int _printf(const char *format, ...);
+int print_octal(unsigned int n);
+int print_unsig(unsigned int n);
+int print_hexaup(unsigned int n);
+int print_hexalow(unsigned int n);
+int print_bi(unsigned int n);
+int _printstring(char *s);
+int rot13(char *point);
+int print_rev(char *);
+int print_hl(uintptr_t n);
+int _withformat4(char c, int count, va_list valist);
+int _withformat3(char c, int count, va_list valist);
+int _withformat2(char c, int count, va_list valist);
+int _withformat(char c, int count, va_list valist);
 #endif
